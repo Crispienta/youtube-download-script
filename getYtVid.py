9d9eaf31a16bd27e8e75beb7a529b8628cf44613
cadrp <121130622+cadrp@users.noreply.github.com>
2023-05-20 23:52:29 +0200

    saving downloads to "download" folder and show progress bar

--- getYtVid.py
+++ getYtVid.py
@@ -1,12 +1,26 @@
 from pytube import YouTube
+from tqdm import tqdm
 import argparse
+import os
 
 def download_video(url):
     try:
-        print("Starting download...")
         youtube = YouTube(url)
         video = youtube.streams.get_highest_resolution()
-        video.download()
+        total_size = video.filesize
+
+        progress_bar = tqdm(total=total_size, unit="B", unit_scale=True)
+
+        # Create the "videos" folder if it doesn't exist
+        if not os.path.exists("videos"):
+            os.makedirs("videos")
+
+        # Download the video to the "videos" folder
+        video.download(output_path="videos", filename=video.default_filename)
+
+        progress_bar.update(total_size - progress_bar.n)
+
+        progress_bar.close()
         print("Video downloaded successfully!")
     except Exception as e:
         print("Error:", str(e))
@@ -17,7 +31,7 @@ def readFile(path):
     file.close()
     return lines
 
-#parsing given arguments
+# Parse given arguments
 argParser = argparse.ArgumentParser(
             prog='getUrls',
             description='Get urls from file or direct url.',
@@ -26,7 +40,7 @@ argParser.add_argument('-u', '--url', help='Url from the video to download from.
 argParser.add_argument('-p', '--path', help='Path to a file containing urls to download from.')
 args = argParser.parse_args()
 
-#check args
+# Check arguments
 if args.url:
     download_video(args.url)
 elif args.path:
